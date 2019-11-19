08cbcad8bbb00fb3c82e5d98eed1edf81b8e1d93
Gaozhikun <17694888671@163.com>
2019-11-19 17:06:48 +0800

    changeDev

--- model/user.js
+++ model/user.js
@@ -1,18 +1,16 @@
-const mongoose = require("mongoose");
-let url = "mongodb://127.0.0.1:27017/yc";
-mongoose.connect(url);
+const mongoose = require('../utils/mongodb').mongoose;
 
+const User = mongoose.model("user", {
+    username: String,
+    password: String
+})
 
-module.exports = {
-    mongoose
-}
 const userFind = (userInfo, cb) => {
-
     User.findOne(userInfo).then(result => {
-
         cb(result)
     })
 }
+
 
 const userSave = (userInfo, cb) => {
     const user = new User(userInfo);
